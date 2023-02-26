4b311b46d9e56584d95bd4a73b5cfb5aa1b5b338
pzproger <pzproger@gmail.com>
2023-02-26 17:11:21 +0300

    step 5

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ validate:
 	composer validate
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
+brain-even:
+	./bin/brain-even
+
---
+++ src/Even.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BrainGames\Even;
+
+use function cli\line;
+use function cli\prompt;
+
+class GameEven
+{
+    public function gameEven(): void
+    {
+        line('Welcome to the Brain Game!');
+        $name = prompt('May I have your name?');
+
+        line("Hello, %s!", $name);
+
+        line('Answer "yes" if the number is even, otherwise answer "no".');
+
+        for ($i = 1; $i < 4; $i++) {
+            $number = self::generateNumber();
+            line('Question:' . $number);
+
+            $answer = prompt('Your answer:');
+//            line("Hello, %s!", $answer);
+
+            if (
+                $number % 2 === 0 && $answer === 'yes'
+                || $number % 2 !== 0 && $answer === 'no'
+            ) {
+                $i == 3 ? line("Congratulations, {$name}!") : line("Correct!");
+            } else {
+                line("{$answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {$name}!");
+                break;
+            }
+        }
+
+    }
+
+    private function generateNumber(int $maxNum = 100): int
+    {
+        return rand(1, $maxNum);
+    }
+}
